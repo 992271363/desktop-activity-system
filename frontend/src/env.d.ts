341ebdf3e7c7ee77ba342245c39f424e74369d96
林洛铭 <992271363@qq.com>
2025-12-03 18:21:12 +0800

    修复2

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,6 +1,10 @@
-// src/shims-vue.d.ts
+// frontend/src/env.d.ts
+
+/// <reference types="vite/client" />
+
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue';
-  const component: DefineComponent;
-  export default component;
+  import type { DefineComponent } from 'vue'
+  // 使用 object 替代 {}，使用 unknown 替代 any
+  const component: DefineComponent<object, object, unknown>
+  export default component
 }
